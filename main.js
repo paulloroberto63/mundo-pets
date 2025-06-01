1208785912aa83ce790353701d1b739776b1d5d6
Paullo Roberto <paulloroberto63@hotmail.com>
2025-06-01 18:51:05 -0300

    instalando e configurando o webpack e o babel

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Configuração do dayjs
+import "./libs/dayjs.js"
+
+// CSS
+import "./styles/form.css"
+import "./styles/global.css"
+import "./styles/icon.css"
+import "./styles/schedule.css"

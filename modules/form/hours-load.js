8504030331340f75721b915ae6c468973598537e
Paullo Roberto <paulloroberto63@hotmail.com>
2025-06-07 13:04:50 -0300

    carregando horário para outros dias

--- modules/form/hours-load.js
+++ modules/form/hours-load.js
@@ -1,11 +1,16 @@
 import dayjs from "dayjs"
-
 import { openingHours } from "../../utils/opening-hours.js"
+import { hoursClick } from "./hours-click.js"
+
+const hours = document.getElementById("time")
 export function hoursLoad({date}){
- const opening = openingHours.map((hour) => {
+ //Limpa a lista de horários
+ hours.innerHTML = ""
+ 
+  const opening = openingHours.map((hour) => {
     // Recupera somente a hora.
     const [scheduleHour] = hour.split(":")
-
+    
     // Adiciona a hora na data e verificar se está no passado.
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
     
@@ -15,4 +20,20 @@ export function hoursLoad({date}){
         available: !isHourPast,
     }
  })
+   
+   // Renderiza os horários.
+   opening.forEach(({hour, available}) =>{
+    const option = document.createElement("option")
+
+    option.classList.add("hour")
+    option.classList.add(available ? "hour-available" : "hour-unavailable" )
+
+    option.textContent = hour
+    
+    hours.append(option)
+   })
+
+   // Adiciona o evento de clique nos horários disponível.
+   hoursClick()
+   
 }
--- modules/form/submit.js
+++ modules/form/submit.js
@@ -1,7 +1,17 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
+const clientPet = document.getElementById("pet")
+const clientTel = document.getElementById("tel")
+const clientDescription = document.getElementById("description")
+
 const selectedDate = document.getElementById("date")
+
+// evento de adicionar apenas numeros no input
+clientTel.addEventListener('input', (event) => {
+    clientTel.value = clientTel.value.replace(/\D/g, '')
+})
 
 // Data atual para formatar o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
@@ -15,5 +25,50 @@ form.onsubmit = (event) => {
     // Previne o comportamento padrão de carregar a página
     event.preventDefault()
      
+    try {
+       const name = clientName.value.trim()
+       const pet = clientPet.value.trim()
+       const tel = clientTel.value.trim()
+       const description = clientDescription.value.trim()
+
+       if(!name) {
+        return alert("Informe o nome do cliente")
+       }
+       if(!pet) {
+        return alert("Informe o pet do cliente")
+       }
+       if(!tel) {
+        return alert("Informe o numero de telefone  do cliente")
+       }
+       if(!description) {
+        return alert("Informe a descrição do agentamento do cliente")
+       }
+
+       // Recupera o horário selecionado.
+       const currentlySelectedHour = document.querySelector(".hourSelect")
+       const selectedOption = currentlySelectedHour.options[currentlySelectedHour.selectedIndex]
+      
+       //recupera somente a hora
+       const [hour] = selectedOption.innerText.split(":")
+ 
+       
+     // Insere a hora da data
+     const when = dayjs(selectedDate.value).add(hour,"hour")
+     
+    // Gera um ID
+    const id = new Date().getMinutes()
+
+    console.log({
+        id,
+        name,
+        pet,
+        tel,
+        description,
+        when
+})
+    } catch (error) {
+        alert("Não foi possivel realizar o agendamento")
+        console.log(error)
+    }
 
 }
